352f059f97f18d09c20df3371d35f32e5579bd5f
Luca Burgio <burgio.luca@gmail.com>
2026-04-18 11:49:15 +0300

    work on units

--- src/scenarios.ts
+++ src/scenarios.ts
@@ -3,10 +3,12 @@ import trainingIcon from '../public/icons/scenarios/training.svg';
 import usWw2Icon from '../public/icons/scenarios/us-ww2.svg';
 import deWw2Icon from '../public/icons/scenarios/de-ww2.svg';
 import ruWw2Icon from '../public/icons/scenarios/ru-ww2.svg';
+import jpWw2Icon from '../public/icons/scenarios/jp-ww2.svg';
 import usvietnamIcon from '../public/icons/scenarios/us-vietnam.svg';
 import trainingImg from '../public/images/scenarios/training.png';
 import usww2Img from '../public/images/scenarios/us-ww2.png';
 import ruww2Img from '../public/images/scenarios/ru-ww2.png';
+import jpww2Img from '../public/images/scenarios/jp-ww2.png';
 import deww2Img from '../public/images/scenarios/de-ww2.png';
 import usvietImg from '../public/images/scenarios/us-vietnam.png';
 
@@ -42,6 +44,14 @@ export const SCENARIOS: ScenarioDef[] = [
     image: ruww2Img,
     miniTitle: 'SECOND WORLD WAR - URSS',
     description: 'The largest conflict in human history. Command Russian forces across a series of tactical engagements on the Eastern front.',
+  },
+  {
+    id: 'ww2jp',
+    icon: jpWw2Icon,
+    title: 'THE RISING SUN',
+    image: jpww2Img,
+    miniTitle: 'SECOND WORLD WAR - JAPAN',
+    description: 'The largest conflict in human history. Command Japan forces across a series of tactical engagements on the Pacific front.',
   }
 ];
 
